9f005a93e1a24343387a2b100f568b06188922de
Sharathim <rathisha273@gmail.com>
2025-08-24 13:16:30 +0530

    git.ps1

--- NotesDock/git.ps1
+++ NotesDock/git.ps1
@@ -2,8 +2,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-<<<<<<< HEAD
 git remote add origin https://github.com/BharathA1303/NotesDock.git
-=======
->>>>>>> e2b7d87b24f1e5b2ba826645fa15fbde0650b497
 git push -u origin main
